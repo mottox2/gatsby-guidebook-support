95d46138a4946d7ba89d0d698ae15f202b99cd67
mottox2 <y.mottox2@gmail.com>
2019-04-06 03:14:01 +0900

    Prepare dynamic page generation

--- gatsby-portfolio/gatsby-node.js
+++ gatsby-portfolio/gatsby-node.js
@@ -5,3 +5,20 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.createPages = ({ graphql, actions }) => {
+  return graphql(`
+    {
+      allWorksYaml {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `).then(result => {
+    // stringifyの引数はJSONのインデントを設定するもの
+    console.log(JSON.stringify(result, null, 4))
+  })
+}
