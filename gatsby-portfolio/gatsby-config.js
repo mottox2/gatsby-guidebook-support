16168eb0379965e9305bde8d192d9b8c9f5e850b
mottox2 <y.mottox2@gmail.com>
2019-04-05 23:46:35 +0900

    Define yaml

--- gatsby-portfolio/gatsby-config.js
+++ gatsby-portfolio/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: "はじめてのGraphQL",
+    description: "はじめてのGraphQLを書いてみましょう",
+    author: "@mottox2",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -11,6 +11,13 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `works`,
+        path: `${__dirname}/src/works.yaml`,
       },
     },
     `gatsby-transformer-sharp`,
